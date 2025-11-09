c4d4961fac9c611a92d616a6afd17ec737ba0be5
Joe Wigmar <joewigmar@gmail.com>
2025-11-09 19:16:59 -0300

    style: add logo, links, login button and style into Navbar.jsx

--- landing-page-react-tailwindcss/src/App.jsx
+++ landing-page-react-tailwindcss/src/App.jsx
@@ -4,7 +4,7 @@ import Home from "./pages/Home";
 
 const App = () => {
   return (
-    <div className="">
+    <div className="px-4 md:px-16 lg:px-32">
       
       <Navbar />
       <Home />
--- landing-page-react-tailwindcss/src/components/Navbar.jsx
+++ landing-page-react-tailwindcss/src/components/Navbar.jsx
@@ -2,7 +2,18 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <nav className='pt-4'>
+        <div className='flex items-center justify-between'>
+            <h4 className='text-3xl font-bold text-sky-600 italic'>Logo</h4>
+            <div className='space-x-6'>
+              <a href="" className='hover:underline font-medium'>Home</a>
+              <a href="" className='hover:underline font-medium'>Features</a> 
+              <a href="" className='hover:underline font-medium'>About</a>  
+              <a href="" className='hover:underline font-medium'>Contact</a>
+            </div>
+            <button className='px-4 py-1.5 bg-sky-600 text-amber-300 font-medium hover:bg-sky-800'>Login</button>
+        </div>
+    </nav>
   )
 }
 
