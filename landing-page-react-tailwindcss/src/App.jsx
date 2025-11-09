875a4d6b59dfc179e8f323e06583b2f35d94af8e
Joe Wigmar <joewigmar@gmail.com>
2025-11-09 18:55:11 -0300

    chore: initialize React project with Vite, Tailwind CSS setup, add Navbar component and Home page

---
+++ landing-page-react-tailwindcss/src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+
+const App = () => {
+  return (
+    <div className="">
+      
+      <Navbar />
+      <Home />
+    </div>
+  );
+};
+
+export default App;
